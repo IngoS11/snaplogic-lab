6e4928787f22d285a20fb95bd5e4a6e77f3c6f89
Ingo Sauerzapf <isauerzapf@snaplogic.com>
2024-01-05 21:43:13 -0800

    added sapgw00 entries to /etc/services

--- gcp-images/gcp-groundplex-ubuntu/install-script.sh
+++ gcp-images/gcp-groundplex-ubuntu/install-script.sh
@@ -14,19 +14,6 @@ curl -o groundplex.deb $DOWNLOAD_URL
 # install groundplex
 sudo dpkg -i ./groundplex.deb 
 
-# TODO
-# check if the package installed successfully and if so remove the downloaded file
-# rm ./groundplex.deb
-
-# TODO
-# download the configuration file from snaplogic from the URL that was provided when creating the groundplex
-# in snapLogic. this currently results in an authentication error when you use the URL if the URL is retreived
-# from SnapLogic more than one hour ago.
-#curl -o /opt/snaplogic/etc/groundplex.slpropz $SPLZ_URL
-# change ownership to snapuser
-#chown snapuser:snapuser /opt/snaplogic/etc/groundplex.slpropz
-#chmod 600 /opt/snaplogic/etc/groundplex.slpropz
-
 # Add environment variable in /etc/sysconfig/jcc for the groundplex to find java
 sudo mkdir -p /etc/sysconfig/
 export JDKPATH=$(find /opt/snaplogic/pkgs -maxdepth 1 -type d -iname "jdk-*")
@@ -58,6 +45,9 @@ EOL'
 
 sudo chmod 664 /lib/systemd/system/snaplogic.service
 
-#sudo systemctl daemon-reload
-#sudo systemctl enable snaplogic
-#sudo systemctl start snaplogic
+# Add /etc/services entries for the SAP gateway. These entries are required for
+# the SAP RFC Listener Snap to find the port of the SAP Gateway. Please add or
+# expand the list here when SAP system numbers other than 00 are needed
+sudo bash -c 'echo "SAP Gateway entries for the SAP RFC Listener Snap to find the SAP Gateway" >> /etc/services'
+sudo bash -c 'echo "sapgw00         3300/tcp                        # SAP System Gateway Port" >> /etc/services'
+sudo bash -c 'echo "sapgw00s        4800/tcp                        # SAP System Gateway Security Port" >> /etc/services'
